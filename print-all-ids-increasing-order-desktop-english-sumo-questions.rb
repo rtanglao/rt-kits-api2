08dde494440468760d5171292459bccf196e0633
Roland Tanglao <roland@rolandtanglao.com>
2019-06-06 23:32:26 -0700

    Update print-all-ids-increasing-order-desktop-english-sumo-questions.rb

--- print-all-ids-increasing-order-desktop-english-sumo-questions.rb
+++ print-all-ids-increasing-order-desktop-english-sumo-questions.rb
@@ -11,17 +11,22 @@ require 'logger'
 logger = Logger.new(STDERR)
 logger.level = Logger::DEBUG
 
-if ARGV.length < 1
-  puts "usage: #{$0} [sumoquestions.csv]"  
+if ARGV.length < 2
+  puts "usage: #{$0} [sumoquestions.csv] [markdown|ids]"  
   exit
 end
+
+markdown = ARGV[1] == "markdown" ? true : false
+ids = ARGV[1] == "ids" ? true : false
+exit if !markdown && !ids
 
 FILENAME = ARGV[0]
 
 id_array = []
 CSV.foreach(FILENAME, :headers => true) do |row|
   next if row['locale'] != "en-US" || row['product'] != 'firefox'
-  id_array.push row['id'] 
+  id_array.push(row['id'].to_s) if ids
+  id.array.push("* https://support.mozilla.org/questions/" + row['id'].to_s) if markdown
 end
 sorted_array = id_array.sort
 logger.debug sorted_array
